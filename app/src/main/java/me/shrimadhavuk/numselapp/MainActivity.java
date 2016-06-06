f6d20a7b244d746b31e1833397b2f968e9174cc8
Shrimadhav U K <spechide@shrimadhavuk.me>
2016-06-06 21:12:45 +0530

    intentfilter

--- app/src/main/java/me/shrimadhavuk/numselapp/MainActivity.java
+++ app/src/main/java/me/shrimadhavuk/numselapp/MainActivity.java
@@ -6,16 +6,20 @@ import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     Button btn1;
     Button btn2;
     EditText txt1;
+    String WHATSAPP_PKG_NAME = "com.whatsapp";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,16 +29,37 @@ public class MainActivity extends AppCompatActivity {
         btn1 = (Button) findViewById(R.id.button);
         btn2 = (Button) findViewById(R.id.button2);
         txt1 = (EditText) findViewById(R.id.editText);
+        Intent intent = getIntent();
+        String action = intent.getAction();
+
+        if(action.equals("android.intent.action.VIEW")) {
+            listenIntent(intent);
+        }
 
     }
 
+    public void listenIntent(Intent intent){
+        // http://stackoverflow.com/questions/2958701/launch-custom-android-application-from-android-browser
+        Uri data = intent.getData();
+        String scheme = data.getScheme();
+        if(scheme.equals("me.shrimadhavuk.whatsapp")){
+            if(isPackageInstalled(WHATSAPP_PKG_NAME)){
+                String phoneno = data.getHost();
+                openWhatsappContact(phoneno);
+            }
+            else{
+                Toast.makeText(MainActivity.this, "WhatsApp not installed", Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
     public void abtAcnpfm(View v){
-        Toast.makeText(MainActivity.this, "https://shrimadhavuk.me", Toast.LENGTH_LONG).show();
+        Toast.makeText(MainActivity.this, "https://github.com/SpEcHiDe/NoWhatOpen", Toast.LENGTH_LONG).show();
     }
 
     public void launchAcnPfm(View v){
         String phonenumber = txt1.getText().toString();
-        if(isPackageInstalled("com.whatsapp")){
+        if(isPackageInstalled(WHATSAPP_PKG_NAME)){
             openWhatsappContact(phonenumber);
         }
     }
@@ -43,7 +68,7 @@ public class MainActivity extends AppCompatActivity {
         // http://stackoverflow.com/questions/15462874/sending-message-through-whatsapp
         Uri uri = Uri.parse("smsto:" + number);
         Intent i = new Intent(Intent.ACTION_SENDTO, uri);
-        i.setPackage("com.whatsapp");
+        i.setPackage(WHATSAPP_PKG_NAME);
         startActivity(Intent.createChooser(i, ""));
     }
 
